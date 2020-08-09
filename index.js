8037afeb40240e2dabdf4dc6eef61bbf47cf4579
ashish-topnozz <69422991+ashish-topnozz@users.noreply.github.com>
2020-08-09 19:39:53 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');  // imported express
+var app = express();   //  created express object 
+
+var myroutes=require('./routes//myRoute.js');   // imported route 
+   
+     
+app.use('/api',myroutes); 
+
+app.listen(3003,()=>{            // used port 3003
+
+    console.log("listening at port 3003");
+})
